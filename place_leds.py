1f72f304bff0c34f3c9cdbe981b9c24c12dbeb27
Neil Enns <neile@live.com>
2021-11-25 20:55:19 -0800

    Add driver directly to schematic

--- place_leds.py
+++ place_leds.py
@@ -7,12 +7,10 @@ from pcbnew import *
 filename="12x16-LED-Matrix.kicad_pcb"
 
 pcb = LoadBoard(filename)
-i = 16*12
 for module in pcb.GetModules():
   if re.search("^D", module.GetReference()):
-    module.SetReference("D%d"%i)
-    module.Reference().SetVisible(False)
+    # module.Reference().SetVisible(False)
+    module.SetLayer(layertable["F.Fab"])
     print("* Module: %s"%module.GetReference())
-    i-=1
 pcb.Save(filename)
 
