37d58548188263078e1cebdf3964c865b711c1be
Neil Enns <{ID}+{username}@users.noreply.github.com>
2021-11-26 05:09:52 +0000

    Move all LED references to the fab layer

--- place_leds.py
+++ place_leds.py
@@ -7,10 +7,20 @@ from pcbnew import *
 filename="12x16-LED-Matrix.kicad_pcb"
 
 pcb = LoadBoard(filename)
+
+def get_layertable():
+    layertable = {}
+    numlayers = PCB_LAYER_ID_COUNT
+    for i in range(numlayers):
+        layertable[pcb.GetLayerName(i)] = i
+    return layertable
+
+layertable = get_layertable()
+
 for module in pcb.GetModules():
   if re.search("^D", module.GetReference()):
     # module.Reference().SetVisible(False)
-    module.SetLayer(layertable["F.Fab"])
+    module.Reference().SetLayer(layertable["F.Fab"])
     print("* Module: %s"%module.GetReference())
 pcb.Save(filename)
 
